1a15361fd596246d25e32b3a0458824901ddf256
anonym0us3 <nathan.shayevich@gmail.com>
2015-11-25 13:54:00 -0800

    minor changes (trying to get background to be transparent

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // wait for DOM to load before running JS
-console.log("Sanity Check = Eez Alive");
+console.log("Sanity Check = Braaaaaaiiiiiiins");
 $(window).ready( function ( event ) {
   
   // your code here
@@ -8,22 +8,22 @@ $('.btn').on("click", function handleClick() {
 	$('.box').empty();
 });
 
-var dog = '<img src="http://i.imgur.com/tbtS98N.png">';
-var cat = '<img src="http://i.imgur.com/17mhzBU.png">';
+// var dog = '<img src="http://i.imgur.com/tbtS98N.png">';
+// var cat = '<img src="http://i.imgur.com/17mhzBU.png">';
 
-var count = 0;
-$('.box').click(function() {
-	if($(this).html() === "") {
-		if (count % 2 === 0) {
-			$(this).html(dog);
-		} else {
-			$(this).html(cat);     	
-     	}
-     	count++;
-     	// console.log(count); used for debugging to make sure clicks were countified 
-    } else {
-    	alert("Negative, Ghost Rider, the pattern is full!");
-    }
-});
+// var count = 0;
+// $('.box').click(function() {
+// 	if($(this).html() === "") {
+// 		if (count % 2 === 0) {
+// 			$(this).html(dog);
+// 		} else {
+// 			$(this).html(cat);     	
+//      	}
+//      	count++;
+//      	// console.log(count); used for debugging to make sure clicks were countified 
+//     } else {
+//     	alert("Negative, Ghost Rider, the pattern is full!");
+//     }
+// });
 
 });
